c2d92e6aab7a7fccf38bc9f2951d61e1a60e7ccf
Steve Burton <stevevburton@gmail.com>
2017-01-31 15:56:28 -0800

    add content for about page

--- src/app/trail.model.ts
+++ src/app/trail.model.ts
@@ -1,3 +1,3 @@
 export class Trail {
-  constructor (public name: string, public description: string, public region: string, public difficulty: string, public length: string, public trailhead-location: string, public picture: string) {}
+  constructor (public name: string, public intro: string, public description: string, public region: string, public difficulty: string, public length: string, public trailhead_location: string, public picture: string) {}
 }
