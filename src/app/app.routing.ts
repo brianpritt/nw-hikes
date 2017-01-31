b26f232e3368a2cd48ed7b1045dda1081b04d9e8
Brian Pritt <brianpritt@gmail.com>
2017-01-31 14:36:53 -0800

    fix commit issues

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,17 +17,10 @@ import { EmailComponent } from './email/email.component';
 
 
 const appRoutes = [
-<<<<<<< HEAD
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path:'message-board',component: MessageBoardComponent},
-  {path: 'profile',component: ProfileComponent},
-=======
   { path: '', component: HomeComponent},
   { path: 'about', component: AboutComponent},
   { path:'message-board',component: MessageBoardComponent},
   { path: 'profile', component: ProfileComponent},
->>>>>>> cc136783f8eab19c4592700966f39a622b163a8c
   { path: 'signup', component: SignupComponent },
   { path: 'login-email', component: EmailComponent },
   { path: 'members', component: OtherComponent, canActivate: [AuthGuard] },
