daea1b8f0d2a50f560977f633781d406604e503d
Brian Pritt <brianpritt@gmail.com>
2017-02-01 11:21:52 -0800

    messy code

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -15,19 +15,32 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
 export class CommentsComponent implements OnInit {
   constructor(private memberService: MemberService,private route: ActivatedRoute,
   private location: Location, private commentsService: CommentsService) { }
+
   routeId;
   todisplay;
-  getComments(){
-    this.todisplay=this.commentsService.getComments().subscribe(data=>{this.todisplay = data; console.log(this.todisplay)});
-  }
-    // console.log(this.commentsService.getComments());
-    // .subscribe(data=>{this.todisplay = data;
+  allComments;
+  routeComments: any = [];
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.routeId = urlParameters['id'];
     });
-    console.log(this.routeId);
-  }
+    this.todisplay = this.commentsService.getComments().subscribe(data=>{this.todisplay = data; console.log(this.todisplay);
 
+    for(var i = 0; i<this.todisplay.length; i++){
+      console.log(this.todisplay[i].hikeId);
+      console.log(this.routeId);
+      if(this.todisplay[i].hikeId == this.routeId){
+        this.routeComments.push(this.todisplay[i])
+      }
+      console.log(this.routeComments)
+    }});
+
+  }
+whatisroutecomments(){
+  console.log(this.routeComments[0].body);
+}
+// newComment(comment:string){
+//   var newComment: Comment = new Comment()
+// }
 }
